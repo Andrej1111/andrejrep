c94b3c45be2fc8af37f1d8e9f0696c437d631f76
McCava <andrej.jelisejev@gmail.com>
2019-03-20 12:48:00 +0200

    antras commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Labas! ąčęėįšųūž ĄČĘĖĮŠŲŪŽ");
+        System.out.println("Morgen! üöäß ÜÖÄß");
+        System.out.println("Здравсвуй!  ЁёЖжФфЯяЧчИиЬьЮюБбЛлЗзЩщШшЪъГгЙйЦц");
+        System.out.println("éöàäèü$£?'`^^¨¨§ﬁ±“#Ç[]|{}≠¿´≤¥≈©√∫~~«…–åß∂ƒ@ªº∆¬¢æœ∑€®†Ω°¡øπ§‘");
     }
 }
